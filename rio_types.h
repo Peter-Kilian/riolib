a16ba9bf1ce35b58e4f097dac1ea4431119528a3
Peter Kilian <peterjohn050@pm.me>
2026-02-14 14:07:54 +0800

    add rio types and directories

---
+++ rio_types.h
@@ -0,0 +1,12 @@
+/*
+ * rio_types.h - v0.01 - Better type naming conventions and additions
+ * 
+ * Standard libraries:
+ * 
+ *     string.h     - string
+ *     stddef.h     - size_t 
+ * 
+ * Written by Peter Kilian
+ */
+
+
